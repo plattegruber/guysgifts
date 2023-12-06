1782701d24e7afd1f689a7cb1722a8805c7ea199
plattegruber <gruberplatte@gmail.com>
2023-12-05 21:11:48 -0600

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
 	import '../app.css';
 	import Nav from './Nav.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Nav></Nav>
