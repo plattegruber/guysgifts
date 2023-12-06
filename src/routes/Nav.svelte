599215533612d2b3e5b5ef8e1dd1ac09102dc7bd
plattegruber <gruberplatte@gmail.com>
2023-12-05 22:03:14 -0600

    dirty fix for mobile menu

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -1,7 +1,24 @@
+<script>
+	const menuStates = {
+		Open: 'open',
+		Closed: 'closed'
+	};
+	let currentMenuState = menuStates.Closed;
+
+	/**
+	 * @return {void}
+	 */
+	const toggleMenuState = () => {
+		currentMenuState == menuStates.Open
+			? (currentMenuState = menuStates.Closed)
+			: (currentMenuState = menuStates.Open);
+	};
+</script>
+
 <header class="bg-white">
 	<nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
 		<div class="flex lg:flex-1">
-			<a href="#" class="-m-1.5 p-1.5">
+			<a href="/" class="-m-1.5 p-1.5">
 				<span class="sr-only">Guys Gifts</span>
 				<img
 					class="h-8 w-auto"
@@ -14,6 +31,7 @@
 			<button
 				type="button"
 				class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+				on:click={toggleMenuState}
 			>
 				<span class="sr-only">Open main menu</span>
 				<svg
@@ -33,58 +51,64 @@
 			</button>
 		</div>
 		<div class="hidden lg:flex lg:gap-x-12">
-			<a href="#" class="text-sm font-semibold leading-6 text-gray-900">Home</a>
+			<a href="/" class="text-sm font-semibold leading-6 text-gray-900">Home</a>
 		</div>
 		<div class="hidden lg:flex lg:flex-1 lg:justify-end">
 			<div class="text-sm font-semibold leading-6 text-gray-900"></div>
 		</div>
 	</nav>
 	<!-- Mobile menu, show/hide based on menu open state. -->
-	<div class="lg:hidden" role="dialog" aria-modal="true">
-		<!-- Background backdrop, show/hide based on slide-over state. -->
-		<div class="fixed inset-0 z-10"></div>
-		<div
-			class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
-		>
-			<div class="flex items-center justify-between">
-				<a href="#" class="-m-1.5 p-1.5">
-					<span class="sr-only">Guys Gifts</span>
-					<img
-						class="h-8 w-auto"
-						src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-						alt=""
-					/>
-				</a>
-				<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
-					<span class="sr-only">Close menu</span>
-					<svg
-						class="h-6 w-6"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						aria-hidden="true"
+	{#if currentMenuState === menuStates.Open}
+		<div class="lg:hidden" role="dialog" aria-modal="true">
+			<!-- Background backdrop, show/hide based on slide-over state. -->
+			<div class="fixed inset-0 z-10"></div>
+			<div
+				class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+			>
+				<div class="flex items-center justify-between">
+					<a href="/" class="-m-1.5 p-1.5">
+						<span class="sr-only">Guys Gifts</span>
+						<img
+							class="h-8 w-auto"
+							src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+							alt=""
+						/>
+					</a>
+					<button
+						type="button"
+						class="-m-2.5 rounded-md p-2.5 text-gray-700"
+						on:click={toggleMenuState}
 					>
-						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-					</svg>
-				</button>
-			</div>
-			<div class="mt-6 flow-root">
-				<div class="-my-6 divide-y divide-gray-500/10">
-					<div class="space-y-2 py-6">
-						<a
-							href="#"
-							class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-							>Home</a
+						<span class="sr-only">Close menu</span>
+						<svg
+							class="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							aria-hidden="true"
 						>
-					</div>
-					<div class="py-6">
-						<div
-							class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-						></div>
+							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</button>
+				</div>
+				<div class="mt-6 flow-root">
+					<div class="-my-6 divide-y divide-gray-500/10">
+						<div class="space-y-2 py-6">
+							<a
+								href="/"
+								class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+								>Home</a
+							>
+						</div>
+						<div class="py-6">
+							<div
+								class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+							></div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</div>
+	{/if}
 </header>
