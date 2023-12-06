b2b0282919a30ab74a72f66152c4daae01faa136
plattegruber <gruberplatte@gmail.com>
2023-12-05 22:55:29 -0600

    add a page descriptor element

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 	{#each data.products as product}
 		<li
-			class="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
+			class="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg text-center shadow bg-white"
 		>
 			<ProductCard
 				name={product.name}
